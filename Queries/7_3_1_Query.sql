bf3958b49de38111f8026180b6fda715f7e1db9c
scottemac00 <scott.e.mccraney@gmail.com>
2022-11-17 22:17:19 -0500

    thru 7.3.1

--- Queries/7_3_1_Query.sql
+++ Queries/7_3_1_Query.sql
@@ -49,4 +49,50 @@ CREATE TABLE titles (
   PRIMARY KEY (emp_no, title, from_date)
 );
 -- Confirm Query
-SELECT * FROM departments
+SELECT * 
+FROM salaries
+
+--Drop Table and retry due to import error
+DROP TABLE dept_emp CASCADE
+
+-- Determine retirement eligibility for employees born b/w 1952 and 1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Refine the query to 1952 only
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Refine query to 1953
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+-- Refine query to 1954
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+-- Refine query to 1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility, refactored and refined
+SELECT COUNT (last_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Export data to a CSV file
+	-- Create a new table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+	-- Verify table info and export by selecting 'Import/Export' from table in Browser
+SELECT *
+FROM retirement_info
--- Queries/PH_Query.sql
+++ Queries/PH_Query.sql
@@ -49,4 +49,39 @@ CREATE TABLE titles (
   PRIMARY KEY (emp_no, title, from_date)
 );
 -- Confirm Query
-SELECT * FROM departments
+SELECT * 
+FROM salaries
+
+--Drop Table and retry due to import error
+DROP TABLE dept_emp CASCADE
+
+-- Determine retirement eligibility for employees born b/w 1952 and 1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Refine the query to 1952 only
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Refine query to 1953
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+-- Refine query to 1954
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+-- Refine query to 1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
